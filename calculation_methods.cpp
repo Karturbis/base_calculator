aa25ac1de2ae107ee857afe97d751b72012f5f2a
karturbis <118579678+Karturbis@users.noreply.github.com>
2025-02-05 21:10:11 +0100

    change variable and function names, so they are number system independent

--- calculation_methods.cpp
+++ calculation_methods.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <cmath>
 
-int base_10(int base_2_number[8]) {
-  int base_10_number = 0;
+int to_decimal(int binary_number[8]) {
+  int decimal_number = 0;
   for (int i = 7; i >= 0; i--) {
-    if (base_2_number[i]){
-      base_10_number += pow(2, 7-i);
+    if (binary_number[i]){
+      decimal_number += pow(2, 7-i);
     }
   }
-  return base_10_number;
+  return decimal_number;
 }
 
 
 int main() {
     int input_number[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-    int s = base_10(input_number);
+    int s = to_decimal(input_number);
     std::cout<< s <<std::endl;
     return 0;
 }
