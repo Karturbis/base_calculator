7357d2f65fd1535fd8f587d08c136ae54e881854
karturbis <118579678+Karturbis@users.noreply.github.com>
2025-02-05 01:55:07 +0100

    initial_commit

---
+++ for_simulation_base_calculator.ino
@@ -0,0 +1,127 @@
+#include <LiquidCrystal.h>
+// declare led pins:
+int led_0 = 6;
+int led_1 = 7;
+int led_2 = 8;
+int led_3 = 9;
+int led_4 = 10;
+int led_5 = 11;
+int led_6 = 12;
+/*
+Analog pins have digital
+names:
+A0 => 14
+A1 => 15
+...
+A5 => 19
+*/
+//declare button pins:
+int button_0 = 19;
+int button_1 = 18;
+int button_enter = 17;
+int switch_number = 16;
+int switch_base = 15;
+
+//declaser display pins:
+LiquidCrystal lcd_1(1, 0, 5, 4, 3, 2);
+
+void setup()
+{
+  // initialize leds:
+  pinMode(led_0, OUTPUT);
+  pinMode(led_1, OUTPUT);
+  pinMode(led_2, OUTPUT);
+  pinMode(led_3, OUTPUT);
+  pinMode(led_4, OUTPUT);
+  pinMode(led_5, OUTPUT);
+  pinMode(led_6, OUTPUT);
+ 
+  // init buttons:
+  pinMode(button_0, INPUT);
+  pinMode(button_1, INPUT);
+  pinMode(button_enter, INPUT);
+  pinMode(switch_number, INPUT);
+  pinMode(switch_base, INPUT);
+  
+  //init lcd (columns, rows):
+  lcd_1.begin(16, 2);
+  // Print hello world
+  lcd_1.print("hello world!");
+}
+
+void loop()
+{
+  bool inputing = true;
+  int enter_input = 0;
+  int button_0_input = 0;
+  int button_1_input = 0;
+  int input_number[7] = {0, 0, 0, 0, 0, 0};
+  int input_counter = 0;
+  while (true) {
+    button_0_input = digitalRead(button_0);
+    button_1_input = digitalRead(button_1);
+    if (button_0_input) {
+      input_number[input_counter] = 0;
+      input_counter = input_counter + 1;
+    }
+    if (button_1_input) {
+      input_number[input_counter] = 1;
+      input_counter = input_counter + 1;
+    }
+    enter_input = digitalRead(button_enter);
+    if (enter_input) {
+      inputing = false;
+    }
+    
+    if(input_number[0]){
+      digitalWrite(led_0, HIGH);
+    }else {
+      digitalWrite(led_0, LOW);
+    }
+      if(input_number[1]){
+      digitalWrite(led_1, HIGH);
+    }else {
+      digitalWrite(led_1, LOW);
+    }
+      if(input_number[2]){
+      digitalWrite(led_2, HIGH);
+    }else {
+      digitalWrite(led_2, LOW);
+    }
+      if(input_number[3]){
+      digitalWrite(led_3, HIGH);
+    }else {
+      digitalWrite(led_3, LOW);
+    }
+      if(input_number[4]){
+      digitalWrite(led_4, HIGH);
+    }else {
+      digitalWrite(led_4, LOW);
+    }
+      if(input_number[5]){
+      digitalWrite(led_5, HIGH);
+    }else {
+      digitalWrite(led_5, LOW);
+    }
+      if(input_number[6]){
+      digitalWrite(led_6, HIGH);
+    }else {
+      digitalWrite(led_6, LOW);
+    }
+    delay(100);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
