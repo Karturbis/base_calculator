fb16e436d414af53131837de4fb43c32e07eee10
karturbis <118579678+Karturbis@users.noreply.github.com>
2025-02-05 23:42:54 +0100

    add decimal to smaller function, to convert numbers from decimal to smaller bases

--- calculation_methods.cpp
+++ calculation_methods.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+using namespace std;
 
-int to_decimal(int binary_number[8]) {
+
+void print_vector(vector<int> input){
+    int vector_size = static_cast<int>(input.size());
+    for(int i = 0; i < vector_size; i++) {
+        cout<< input[i] <<" ";
+    }
+    cout<< "\n";
+}
+
+int binary_to_decimal(int binary_number[8]) {
   int decimal_number = 0;
   for (int i = 7; i >= 0; i--) {
     if (binary_number[i]){
@@ -11,11 +22,28 @@ int to_decimal(int binary_number[8]) {
   return decimal_number;
 }
 
+vector<int> decimal_to_smaller(int decimal_number, int new_base) {
+    int remainder = 0;
+    int quotient = decimal_number;
+    vector<int> new_number = {};
+
+    for (; quotient > 0;){
+        remainder = quotient % new_base;
+        quotient = (int) quotient / new_base;
+        new_number.push_back(remainder);
+    }
+    return new_number;
+}
+
+
 
 int main() {
     int input_number[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-    int s = to_decimal(input_number);
-    std::cout<< s <<std::endl;
+    int decimal_number = binary_to_decimal(input_number);
+    cout<< "Binary to decimal:" <<endl;
+    cout<< decimal_number <<endl;
+    vector<int> test = decimal_to_smaller(55, 4);
+    print_vector(test);
     return 0;
 }
 
