9b1eac19359c73d00ef45baf9ea2f759fc0d15f3
karturbis <118579678+Karturbis@users.noreply.github.com>
2025-02-05 02:04:14 +0100

    add one led, remove unnecessary if statements

--- for_simulation_base_calculator.ino
+++ for_simulation_base_calculator.ino
@@ -7,6 +7,7 @@ int led_3 = 9;
 int led_4 = 10;
 int led_5 = 11;
 int led_6 = 12;
+int led_7 = 13;
 /*
 Analog pins have digital
 names:
@@ -35,6 +36,7 @@ void setup()
   pinMode(led_4, OUTPUT);
   pinMode(led_5, OUTPUT);
   pinMode(led_6, OUTPUT);
+  pinMode(led_7, OUTPUT);
  
   // init buttons:
   pinMode(button_0, INPUT);
@@ -55,17 +57,17 @@ void loop()
   int enter_input = 0;
   int button_0_input = 0;
   int button_1_input = 0;
-  int input_number[7] = {0, 0, 0, 0, 0, 0};
+  int input_number[8] = {0, 0, 0, 0, 0, 0, 0};
   int input_counter = 0;
   while (true) {
     button_0_input = digitalRead(button_0);
     button_1_input = digitalRead(button_1);
     if (button_0_input) {
-      input_number[input_counter] = 0;
+      input_number[input_counter] = LOW;
       input_counter = input_counter + 1;
     }
     if (button_1_input) {
-      input_number[input_counter] = 1;
+      input_number[input_counter] = HIGH;
       input_counter = input_counter + 1;
     }
     enter_input = digitalRead(button_enter);
@@ -73,41 +75,15 @@ void loop()
       inputing = false;
     }
     
-    if(input_number[0]){
-      digitalWrite(led_0, HIGH);
-    }else {
-      digitalWrite(led_0, LOW);
-    }
-      if(input_number[1]){
-      digitalWrite(led_1, HIGH);
-    }else {
-      digitalWrite(led_1, LOW);
-    }
-      if(input_number[2]){
-      digitalWrite(led_2, HIGH);
-    }else {
-      digitalWrite(led_2, LOW);
-    }
-      if(input_number[3]){
-      digitalWrite(led_3, HIGH);
-    }else {
-      digitalWrite(led_3, LOW);
-    }
-      if(input_number[4]){
-      digitalWrite(led_4, HIGH);
-    }else {
-      digitalWrite(led_4, LOW);
-    }
-      if(input_number[5]){
-      digitalWrite(led_5, HIGH);
-    }else {
-      digitalWrite(led_5, LOW);
-    }
-      if(input_number[6]){
-      digitalWrite(led_6, HIGH);
-    }else {
-      digitalWrite(led_6, LOW);
-    }
+    digitalWrite(led_0, input_number[0]);
+    digitalWrite(led_1, input_number[1]);
+    digitalWrite(led_2, input_number[2]);
+    digitalWrite(led_3, input_number[3]);
+    digitalWrite(led_4, input_number[4]);
+    digitalWrite(led_5, input_number[5]);
+    digitalWrite(led_6, input_number[6]);
+    digitalWrite(led_7, input_number[7]);
+    
     delay(100);
   }
 }
