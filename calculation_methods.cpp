f050bd3b5ce7477e58c47b7c61ef184de0bcdb93
karturbis <118579678+Karturbis@users.noreply.github.com>
2025-02-10 12:43:48 +0100

    add variables for converting char to int and int to char

--- calculation_methods.cpp
+++ calculation_methods.cpp
@@ -1,55 +1,96 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
+char int_to_char[36] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+unordered_map<char, int> chat_to_int =
+{
+  {'0', 0},
+  {'1', 1},
+  {'2', 2},
+  {'3', 3},
+  {'4', 4},
+  {'5', 5},
+  {'6', 6},
+  {'7', 7},
+  {'8', 8},
+  {'9', 9},
+  {'A', 10},
+  {'B', 11},
+  {'C', 12},
+  {'D', 13},
+  {'E', 14},
+  {'F', 15},
+  {'G', 16},
+  {'H', 17},
+  {'I', 18},
+  {'J', 19},
+  {'K', 20},
+  {'L', 21},
+  {'M', 22},
+  {'N', 23},
+  {'O', 24},
+  {'P', 25},
+  {'Q', 26},
+  {'R', 27},
+  {'S', 28},
+  {'T', 29},
+  {'U', 30},
+  {'V', 31},
+  {'W', 32},
+  {'X', 33},
+  {'Y', 34},
+  {'Z', 35},
+};
 
-void print_vector(vector<int> input){
-    int vector_size = static_cast<int>(input.size());
+
+
+void print_vector(vector<char> input){
+    int vector_size = static_cast<char>(input.size());
     for(int i = 0; i < vector_size; i++) {
         cout<< input[i] <<"";
     }
     cout<< "\n";
 }
 
-int binary_to_decimal(int binary_number[8]) {
+int other_to_decimal(int other_number[8], int base) {
   int decimal_number = 0;
   for (int i = 7; i >= 0; i--) {
-    if (binary_number[i]){
-      decimal_number += pow(2, 7-i);
+    if (other_number[i]){
+      decimal_number += pow(base, 7-i);
     }
   }
   return decimal_number;
 }
 
-vector<int> decimal_to_other(int decimal_number, int new_base) {
+vector<char> decimal_to_other(int decimal_number, int new_base) {
     int remainder = 0;
     int quotient = decimal_number;
     vector<int> new_number = {};
+    vector<char> new_number_char = {};
 
     for (; quotient > 0;){
         remainder = quotient % new_base;
         quotient = (int) quotient / new_base;
         new_number.insert(new_number.begin(), remainder); // inserts remainder at the begin of the number
     }
-/*
-    if (new_base > 10) {
-      int vector_size = static_cast<int>(new_number.size());
-      for (int i = 0; i < vector_size; i++){
-      }
+    int vector_size = static_cast<int>(new_number.size());
+    for (int i = 0; i < vector_size; i++){
+      new_number_char.push_back(int_to_char[new_number[i]]);
     }
-*/
-    return new_number;
+    return new_number_char;
 }
 
 
 int main() {
     int input_number[8] = {1, 0, 1, 0, 0, 0, 1, 0};
-    int decimal_number = binary_to_decimal(input_number);
+    int decimal_number = other_to_decimal(input_number);
     cout<< "Binary to decimal:" <<endl;
     cout<< decimal_number <<endl;
-    int new_base = 11;
-    vector<int> test = decimal_to_other(234, new_base);
+    int new_base = 9;
+    vector<char> test = decimal_to_other(-12, new_base);
     cout<< "Decimal to" << " ";
     cout<< new_base << ":\n";
     print_vector(test);
