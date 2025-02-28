30956bb8080b06b3d153e4ff9502bb28c778d7ba
karturbis <118579678+Karturbis@users.noreply.github.com>
2025-02-28 16:28:57 +0100

    start implementing the different modes

--- interface/interface.ino
+++ interface/interface.ino
@@ -4,21 +4,23 @@
 #include <Keypad.h>
 
 
-String key_input;
-const String modes[4] = {"Menu", "Input base 1", "Input base 2", "Calculate"};
-String mode;
+String user_input;
+int base_1[3];
+int base_2[3];
+const char modes[4] = {'m', '1', '2', 'c'};
+char mode;
 
 // declare display pins
-const int rs = 1;
-const int en = 0;
+const int rs = 0;
+const int en = 1;
 const int data_0 = 8;
 const int data_1 = 9;
 const int data_2 = 10;
 const int data_3 = 11;
-const int data_4 = 5;
-const int data_5 = 4;
-const int data_6 = 3;
-const int data_7 = 2;
+const int data_4 = 2;
+const int data_5 = 3;
+const int data_6 = 4;
+const int data_7 = 5;
 
 // init display pins:
 LiquidCrystal lcd(rs, en, data_4, data_5, data_6, data_7);
@@ -41,11 +43,29 @@ void setup() {
   //init lcd (columns, rows):
   lcd.begin(16, 2);
   // print welcome screen
-  lcd.print("Base Number calculator!");
+  lcd.print("Base number calc");
+  delay(1000);
   menu();
 }
 
 void loop() {
+  switch (mode) {
+    case 'm':
+      menu();
+      break;
+    case '1':
+      input_base_1();
+      break;
+    case '2':
+      input_base_2();
+      break;
+    case 'c':
+      calculate();
+      break;
+    default:
+      menu();
+      break;
+  }
 }
 
 void input() {
@@ -58,18 +78,46 @@ void input() {
       if (key == '#') {
         break;
       }
-      key_input = String(key_input + key);
+      if (key == 'A') {
+        lcd.clear();
+        mode = "menu";
+        user_input = "";
+        break;
+      }
+      user_input = String(user_input + key);
     }
     lcd.setCursor(0, 1);
-    lcd.print(key_input);
+    lcd.print(user_input);
     delay(50);
  }
 
 }
 
 void menu() {
-  mode = "menu";
+  mode = 'm';
   input();
   lcd.clear();
-  lcd.print(key_input);
+  lcd.print(user_input);
+  mode = modes[user_input.toInt()];
+}
+
+void input_base_1() {
+  mode = '1';
+  input();
+
+  //base_1 = {};
+
+}
+
+void input_base_2() {
+  mode = '2';
+  input();
+  lcd.clear();
+  lcd.print("Base 1 is:");
+  lcd.setCursor(0, 1);
+  lcd.print(user_input);
+}
+
+void calculate() {
+  // add calculation code
 }
