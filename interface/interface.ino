13df1aaca91cfc8114914bba18a2326eb086a8f0
Hyfan100 <160237062+Hyfan100@users.noreply.github.com>
2025-03-12 02:36:27 +0100

    Viel

--- interface/interface.ino
+++ interface/interface.ino
@@ -3,6 +3,7 @@
 #include <LiquidCrystal.h>
 #include <Keypad.h>
 
+
 using namespace std;
 
 const char int_to_char[36] =
@@ -10,12 +11,13 @@ const char int_to_char[36] =
   '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 };
 
-int base_from_numerator[3];
-int base_from_denominator[3];
-int base_to_numerator[3];
-int base_to_denominator[3];
+int base_1_numerator = 10;
+int base_1_denominator = 1;
+int base_2_numerator = 2;
+int base_2_denominator = 1;
+
 String mode = "menu";
-int input_length = 16;
+const int input_length = 16;
 
 // declare display pins
 const byte rs = 0;
@@ -40,11 +42,13 @@ char keys[ROW_NUMBER][COLUMN_NUMBER] = {
 byte pin_rows[ROW_NUMBER] = {13, 12, 11, 10};
 byte pin_columns[COLUMN_NUMBER] = {9, 8, 7, 6};
 
-// key meanings
+//key meanings
 const char up = 'A';
 const char down = 'B';
 const char select = '#';
 const char menu_key = 'C';
+
+
 // init keypad:
 Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_columns, ROW_NUMBER, COLUMN_NUMBER);
 
@@ -52,24 +56,30 @@ void setup() {
   //init lcd (columns, rows):
   lcd.begin(16, 2);
   // print welcome screen
-  String msg = "-12number calc";
-  lcd.print(msg.substring(-1));
-  delay(1000);
+  //lcd.print("Base number calc)";
+  //delay(1000);
+  int manum[16]={0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1};
+  //lcd.print(other_to_decimal(manum, 2, 1));
+  char x[16];
+  x = decimal_to_other(1, 2);
+  lcd.print(x);
 }
 
 void loop() {
-  if(mode == "menu") {
-    menu();
-  }
-  else {
-    set_base(1);
-    lcd.clear();
-    lcd.print("not implemented");
-    lcd.setCursor(0, 1);
-    lcd.print("return to menu");
-    mode = "menu";
-    delay(2000);
-  }
+  // if(mode == "menu") {
+  //   menu();
+  // } else if(mode == "base 1"){
+  //   set_base(1);
+  // } else if(mode == "base 2"){
+  //   set_base(2);
+  // } else {
+  //   lcd.clear();
+  //   lcd.print("not implemented");
+  //   lcd.setCursor(0, 1);
+  //   lcd.print("return to menu");
+  //   mode = "menu";
+  //   delay(2000);
+  // }
 }
 
 String get_input(String msg) {
@@ -95,11 +105,12 @@ String get_input(String msg) {
       }
       delay(50);
   }
+
 }
 
 char japanese_ogre_chosen_one(){
   const byte optnum = 3;
-  const char options[optnum] = {'-', '/', ' '};
+  const char options[optnum] = {'/', '-', ' '};
   byte japanese_ogre_location = 0;
   bool japanese_ogre_direction = true; //true = top, false = bottom
 
@@ -187,26 +198,47 @@ void calculate() {
 }
 
 void set_base(int the_japanese_ogres_wanted_to_know_what_base_to_change_komma_so_the_japanese_ogres_crafted_this_variable_period_it_apostrophe_s_purpose_is_telling_the_function_what_base_to_change_period){
-  String user_input = (get_input("Base " + String(the_japanese_ogres_wanted_to_know_what_base_to_change_komma_so_the_japanese_ogres_crafted_this_variable_period_it_apostrophe_s_purpose_is_telling_the_function_what_base_to_change_period) + ":"));
+  mode = "menu";
+  String user_input = get_input("Base " + String(the_japanese_ogres_wanted_to_know_what_base_to_change_komma_so_the_japanese_ogres_crafted_this_variable_period_it_apostrophe_s_purpose_is_telling_the_function_what_base_to_change_period) + ":");
   lcd.clear();
   if (user_input.indexOf('/', user_input.indexOf('/')+1) != -1) {
     lcd.print("bad / try again");
+    delay(1000);
     return;
   } else{
     bool is_negative = false;
     for(int i = user_input.indexOf('-'); i != -1; i = user_input.indexOf('-', i+1)){
       if(i != 0 && user_input[i-1] != '/'){
         lcd.print("bad - try again");
+        delay(1000);
         return;
       }
     }
   }
-  int numerator = user_input.substring(0, user_input.indexOf('/')).toInt();
-  int denominator = user_input.substring(user_input.indexOf('/')).toInt();
 
+  int slash_index = user_input.indexOf('/');
+  int numerator = user_input.substring(0, slash_index).toInt();
+  int denominator = slash_index + 1? user_input.substring(slash_index + 1).toInt() : 1;
+  if(!numerator || !denominator){
+    lcd.print("input error");
+    delay(1000);
+    return;
+  }
 
-
-
+  if(the_japanese_ogres_wanted_to_know_what_base_to_change_komma_so_the_japanese_ogres_crafted_this_variable_period_it_apostrophe_s_purpose_is_telling_the_function_what_base_to_change_period-1){
+    base_1_numerator = numerator;
+    base_1_denominator = denominator;
+  } else{
+    base_2_numerator = numerator;
+    base_2_denominator = denominator;
+  }
+  
+  
+  
+  lcd.clear();
+  lcd.print(numerator);
+  lcd.setCursor(0, 1);
+  lcd.print(denominator);
   delay(1000);
 }
 
@@ -215,52 +247,51 @@ void set_base(int the_japanese_ogres_wanted_to_know_what_base_to_change_komma_so
 
 // calculation methods:
 
-int other_to_decimal(int other_number[8], int base) {
-  int decimal_number = 0;
-  for (int i = 7; i >= 0; i--) {
-    if (other_number[i]){
-      decimal_number += pow(base, 7-i);
-    }
+float other_to_decimal(int other_number[input_length], int numerator, int denominator) {
+  float decimal_number = 0;
+  for (int i = input_length; i >= 0; i--) { 
+    decimal_number += other_number[i]*(pow(numerator, 15-i)/pow(denominator, input_length-1-i));
   }
   return decimal_number;
 }
 
 char* decimal_to_other(int decimal_number, int new_base, int denominator = 1) {
-    char new_number_char[input_length] = {' ', ' ', ' ', ' ',' ', ' ', ' ', ' ',' ', ' ', ' ', ' ',' ', ' ', ' ', ' '};
-      bool is_negativ;
-      if (decimal_number < 0 && new_base > 0) {
-        is_negativ = true;
-        decimal_number = abs(decimal_number);
-      }
-      else {
-        is_negativ = false;
-      }
-      int remainder = 0;
-      int quotient = decimal_number;
-      int new_number[input_length] = {};
+  char new_number_char[input_length] = {' ', ' ', ' ', ' ',' ', ' ', ' ', ' ',' ', ' ', ' ', ' ',' ', ' ', ' ', ' '}; //japanese ogres where to lazy to do it properly
+  bool is_negativ;
+  if (decimal_number < 0 && new_base > 0) {
+    is_negativ = true;
+    decimal_number = abs(decimal_number);
+  } else {
+    is_negativ = false;
+  }
+  int remainder = 0;
+  int quotient = decimal_number;
+  int new_number[input_length] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}; //japanese ogres where to lazy to do it properly
+  for (int i = input_length-1; quotient != 0; i--){
+    remainder = quotient % new_base; 
+    quotient = (int) quotient / new_base; //japanese ogre magic solution
+    if (remainder < 0) { // important only for negative bases
+      remainder += abs(new_base);
+      quotient ++;
+    }
+    quotient *= denominator;
+    new_number[i] = remainder; // inserts remainder at the begin of the number
+  }
+  for(int i = 0; i < input_length; i++){
+    if(!new_number[i] + 1){
+      new_number_char[i] = int_to_char[new_number[i]];
+    }
+  }
 
-      for (int i = input_length-1; quotient != 0; i--){
-          remainder = quotient % new_base;
-          quotient = (int) quotient / new_base;
-          if (remainder < 0) { // important only for negative bases
-            remainder += abs(new_base);
-            quotient ++;
-          }
-          quotient *= denominator;
-          new_number[i] = remainder; // inserts remainder at the begin of the number
+  if (is_negativ){
+    for (int i = input_length-1; i > 0; i--) {
+      if (!new_number[i] + 1){
+        new_number_char[i] = '-';
+        break;
       }
-      for (int i = input_length-1; new_number[i] != ' ' && i > 0; i--){
-        new_number_char[i] = int_to_char[new_number[i]];
-      }
-      if (is_negativ) {
-        for (int i = input_length-1; i > 0; i--) {
-          if (new_number[i] == ' ') {
-            new_number_char[i] = '-';
-            break;
-          }
-        }
-      }
-      return new_number_char;
+    }
+  }
+  return new_number_char;
 }
 
 char* fractional_bases(int decimal_number, int numerator, int denominator) {
@@ -283,3 +314,12 @@ char* fractional_bases(int decimal_number, int numerator, int denominator) {
     return decimal_to_other(decimal_number, numerator, denominator);
   }
 }
+
+
+
+
+
+
+
+
+
